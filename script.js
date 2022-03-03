75ccaa8243b8e563703e5609fcba0ed1d0093223
DLindeberg <90319103+DLindeberg@users.noreply.github.com>
2022-03-03 01:04:08 +0100

    small changes

--- script.js
+++ script.js
@@ -2,16 +2,24 @@ let enterbutton = document.querySelector("#enterbutton");
 let form = document.querySelector("form");
 let input = document.querySelector("#input");
 
+
+
 form.onsubmit = event => {
     event.preventDefault();
 }
 
 enterbutton.onclick = event => {
+    if (!input.value) {
+        return;
+    }
     const thisTitle = document.createElement("li");
     thisTitle.setAttribute("id", "todolist");
     thisTitle.innerHTML = "<input type='checkbox'>" + input.value + "<button class='delete'>❌</button>";
     document.querySelector("#todo").appendChild(thisTitle)
     addTask();
+    const inputInput = document.getElementById('input');
+    inputInput.value = '';
+    
 }
 
 function addTask() {
@@ -21,6 +29,10 @@ function addTask() {
         close[d].onclick = function () {
             var div = this.parentElement;
             div.remove(div);
+
         }
     }
 }
+
+let count = document.querySelectorAll("#todolist").length;
+document.getElementById("count").innerHTML = count + " items left";
